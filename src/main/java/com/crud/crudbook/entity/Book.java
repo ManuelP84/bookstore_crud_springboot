0a7601d7424f4a05331ebb2370e7a55d687029a5
Manuel Pineda <pineda.manuel@gmail.com>
2022-04-27 16:31:34 -0500

    Initial project ---- CRUD Bookstore in Springboot. Initial commit

---
+++ src/main/java/com/crud/crudbook/entity/Book.java
@@ -0,0 +1,22 @@
+package com.crud.crudbook.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity(name = "Book")
+@Table(name = "book")
+@Data
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long    id;
+    private String  tittle;
+    private String  author;
+    private Integer numberPages;
+    private String    language;
+    private LocalDate datePublished;
+
+}
